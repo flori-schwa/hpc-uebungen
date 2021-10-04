1e8e98ef734741ebc7787429721fb6c3db82df93
Florian <florian.schwabe@hotmail.de>
2021-10-04 12:34:46 +0200

    Übung 1

--- graustufen/graustufen-loop.c
+++ graustufen/graustufen-loop.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+struct grayscale_args {
+    unsigned char* src;
+    int pixels;
+    int channels;
+    unsigned char* target;
+};
+
+void* grayscale_calc(void* v_args) {
+    struct grayscale_args* args = (struct grayscale_args*) v_args;
+
+    if (args->channels < 3) {
+        printf("Image already grascale\n");
+        return NULL;
+    }
+
+    for (int pixel = 0; pixel < args->pixels; pixel++) {
+        int srcOffset = pixel * args->channels;
+        float grayscale = 0.2126f * args->src[srcOffset] + 0.7152f * args->src[srcOffset + 1] + 0.0722f * args->src[srcOffset + 2];
+        args->target[pixel] = (int) grayscale;
+
+        // usleep(20);
+    }
+
+    return NULL;
+}
+
 int main()
 {
     // Read color JPG into byte array "img"
     // Array contains "width" x "height" pixels each consisting of "channels" colors/bytes
     int width, height, channels;
-    unsigned char *img = stbi_load("tamanna-rumee-vaTsR-ghLog-unsplash.jpg", &width, &height, &channels, 0);
+    unsigned char *img = stbi_load("tamanna-rumee-vaTsR-ghLog-unsplash.jpg", &width, &height, &channels, 3);
     if (img == NULL)
     {
         printf("Err: loading image\n");
@@ -26,13 +55,76 @@ int main()
 
     // TODO Zeitmessen Start
 
+    struct timeval start;
+    gettimeofday(&start, 0);
+
     // TODO Konvertierung
+
+#define MULTI
+
+#ifdef SINGLE
+    struct grayscale_args args = {
+        .src = img,
+        .channels = channels,
+        .pixels = width * height,
+        .target = gray
+    };
+
+    grayscale_calc(&args);
+#elif defined(MULTI)
+    #define N_THREADS 8
+
+    struct grayscale_args thread_args[N_THREADS];
+    pthread_t threads[N_THREADS];
+
+    int threadPixels = (width * height) / N_THREADS;
+
+    for (int i = 0; i < N_THREADS; i++) {
+        thread_args[i].src = img + (i * threadPixels * channels);
+        thread_args[i].channels = channels;
+        thread_args[i].target = gray + (i * threadPixels);
+
+        if (i == (N_THREADS - 1)) {
+            thread_args[i].pixels = (width * height) - ((N_THREADS - 1) * threadPixels);
+        } else {
+            thread_args[i].pixels = threadPixels;
+        }
+
+        int err = pthread_create(&threads[i], NULL, grayscale_calc, &thread_args[i]);
+
+        if (err) {
+            printf("Error starting thread #%d: %d\n", (i + 1), err);
+            exit(err);
+        }
+    }
+
+    for (int i = 0; i < N_THREADS; i++) {
+        int err = pthread_join(threads[i], NULL);
+
+        if (err) {
+            printf("Error in thread #%d: %d\n", (i + 1), err);
+            exit(err);
+        }
+    }
+#endif
 
     // TODO Zeitmessen Ende
 
+    struct timeval end;
+    gettimeofday(&end, 0);
+    long lsec = end.tv_sec - start.tv_sec;
+    long lusec = end.tv_usec - start.tv_usec;
+    double sec = (lsec + lusec / 1000000.0);
+    printf("%8.6f seconds\n", sec);
+
     // TODO FLOP Berechnung
 
+    long flop = width * height * (3 + 2);
+
     //TODO Ausgabe FLOP/s
+
+    double mflops = flop / 1000000.0 / sec;
+    printf("%8.2f MFLOP/s\n", mflops);
 
     stbi_write_jpg("grayscale.jpg", width, height, 1, gray, 95);
 }
