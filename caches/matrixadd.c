697125495e8889f6fd1b15cb18ab75b52751a627
Florian <florian.schwabe@hotmail.de>
2021-10-11 12:30:42 +0200

    Matrix multiplication

--- caches/matrixadd.c
+++ caches/matrixadd.c
@@ -5,35 +5,30 @@
 #define X 8192
 #define Y 8192
 
-void **malloc2d(int x, int y, size_t size)
-{
-    void **array = malloc(x * sizeof(void *) + x * y * size);
-    for (int i = 0; i < x; i++)
-    {
-        int offset = x * sizeof(void *) + i * y * size;
-        array[i] = (void *)((unsigned char *)array + offset);
+void** malloc2d(int x, int y, size_t size) {
+    void** array = malloc(x * sizeof(void*) + x * y * size);
+    for (int i = 0; i < x; i++) {
+        int offset = x * sizeof(void*) + i * y * size;
+        array[i] = (void*) ((unsigned char*) array + offset);
     }
     return array;
 }
 
-int main()
-{
-    float **a = (float **)malloc2d(X, Y, sizeof(float));
-    float **b = (float **)malloc2d(X, Y, sizeof(float));
-    
+int main() {
+    float** a = (float**) malloc2d(X, Y, sizeof(float));
+    float** b = (float**) malloc2d(X, Y, sizeof(float));
+
     int i, j;
 
-    printf("sizeof(float) = %lu\n", sizeof(float));
-    printf("Größe der Matrix: %i x %i = %i Elemente\n", X, Y, X * Y);
-    printf("Größe einer Matrix: %lu kBytes\n", sizeof(float) * X * Y / 1024);
-    printf("Größe einer Reihe: %lu kBytes\n", sizeof(float) * X / 1024);
+    printf("sizeof(float) = %llu\n", sizeof(float));
+    printf("Groesse der Matrix: %i x %i = %i Elemente\n", X, Y, X * Y);
+    printf("Groesse einer Matrix: %llu kBytes\n", sizeof(float) * X * Y / 1024);
+    printf("Groesse einer Reihe: %llu kBytes\n", sizeof(float) * X / 1024);
 
-    for (i = 0; i < X; i++)
-    {
-        for (j = 0; j < Y; j++)
-        {
-            a[i][j] = 1.0;
-            b[i][j] = 2.0;
+    for (i = 0; i < X; i++) {
+        for (j = 0; j < Y; j++) {
+            a[i][j] = 1.0f;
+            b[i][j] = 2.0f;
         }
     }
 
@@ -42,11 +37,12 @@ int main()
     gettimeofday(&start, 0);
 
     // TODO: Optimize this loop
-    for (j = 0; j < Y; j++)
-    {
-        for (i = 0; i < X; i++)
-        {
-            a[i][j] += b[i][j];
+    for (i = 0; i < X; i++) {
+        float* aRow = a[i];
+        float* bRow = b[i];
+
+        for (j = 0; j < Y; j++) {
+            aRow[j] += bRow[j];
         }
     }
 
@@ -59,6 +55,9 @@ int main()
 
     printf("%.2f MFLOP\n%.2f sec\n", flop / 1000000.0, secs);
     printf("%.2f MFLOPs\n", flop / secs / 1000000.0);
+
+    free(a);
+    free(b);
 
     return 0;
 }
